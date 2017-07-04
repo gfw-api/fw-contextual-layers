3e5cee9300d50668c82146086fb81c20d50acb9c
Rodrigo Solis <rodrigo.solis@vizzuality.com>
2017-07-04 15:43:57 +0200

    fix: recovers async/await on middlewares

--- app/src/services/layer.service.js
+++ app/src/services/layer.service.js
@@ -9,10 +9,10 @@ class LayerService {
     };
   }
 
-  static async create(data, owner) {
+  static create(data, owner) {
     const isPublic = (data.user.role === 'ADMIN') && (owner.type === 'USER') ? data.isPublic : false;
     const body = Object.assign({}, data, { isPublic, owner });
-    return await new LayerModel(body).save();
+    return new LayerModel(body).save();
   }
 }
 module.exports = LayerService;
